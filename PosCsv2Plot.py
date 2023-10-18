8b949635d20c7f210b48436a2e3e9a877fc10e79
kingbaard <bradley.rod.jensen@gmail.com>
2023-10-18 14:45:12 -0600

    added posCsv2Plot.py

---
+++ PosCsv2Plot.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+x_positions = []
+y_positions = []
+
+with open(sys.argv[1], 'r') as file:
+    csvreader = csv.reader(file)
+    next(csvreader)
+
+    for row in csvreader:
+        x_positions.append(row[1])
+        y_positions.append(row[2])
+
+plt.scatter(x_positions, y_positions)
+plt.title("Robot Positions Plotted Againt X and Y Axis")
+plt.xlabel("X Position")
+plt.ylabel("Y Position")
+plt.show()
