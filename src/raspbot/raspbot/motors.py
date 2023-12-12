3a2d08039a884ffb5b3da54a4f7824aeda6e8a61
kingbaard <bradley.rod.jensen@gmail.com>
2023-12-12 12:17:58 -0700

    ir debugging

--- src/raspbot/raspbot/motors.py
+++ src/raspbot/raspbot/motors.py
@@ -337,7 +337,7 @@ class MinimalSubscriber(Node):
       self.last_irs.append(ir_result)
       self.last_irs = self.last_irs[1:]
       self.is_dark_floor = sum(self.last_irs) > 8
-      # print(f"Is dark floor: {self.is_dark_floor}")
+      print(f"Is dark floor: {self.last_irs}")
 
 class States(Enum):
   SEARCH = 0
