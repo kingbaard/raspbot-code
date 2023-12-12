2d19b858e652ee001d333c198afa1e1d629e1104
kingbaard <bradley.rod.jensen@gmail.com>
2023-12-11 18:36:44 -0700

    GPIO debugging

--- src/raspbot/raspbot/ir.py
+++ src/raspbot/raspbot/ir.py
@@ -24,10 +24,6 @@ class IrPublisher(Node):
         # Set up IR sensors
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
-        GPIO.setup(RIGHT1, GPIO.IN)
-        GPIO.setup(RIGHT2, GPIO.IN)
-        GPIO.setup(LEFT1, GPIO.IN)
-        GPIO.setup(LEFT1, GPIO.IN)
 
         timer_period = 0.5
         self.publisher = self.create_publisher(Bool, 'ir', 10)
@@ -35,6 +31,10 @@ class IrPublisher(Node):
 
     # If at least three sensors publish True, else false
     def ir_callback(self):
+        GPIO.setup(RIGHT1, GPIO.IN)
+        GPIO.setup(RIGHT2, GPIO.IN)
+        GPIO.setup(LEFT1, GPIO.IN)
+        GPIO.setup(LEFT1, GPIO.IN)
         return_count = 0
         for sensor in [RIGHT1, RIGHT2, LEFT1, LEFT2]:
             print(sensor)
